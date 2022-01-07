e0c3d9d9ed2565c3cdaedd098c1a047e42a06cd6
鲁举佩 <653390161@qq.com>
2022-01-07 10:15:57 +0800

    feat: jsdoc

---
+++ README.md
@@ -0,0 +1,13 @@
+<a name="debounce"></a>
+
+## debounce(milliSec, [formatStr]) ⇒ <code>String</code>
+功能：将时间戳格式化为指定格式的字符串
+
+**Kind**: global function  
+**Returns**: <code>String</code> - - 根据目标时间格式，将时间数值（或Date）转换成的时间字符串  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| milliSec | <code>Number</code> | 要转换的时间，可以为秒、毫秒、微秒、或Date类型 |
+| [formatStr] | <code>String</code> | 目标格式字符串 可选 默认为：'yyyy-MM-dd hh:mm:ss' |
+
---
+++ scripts/jsdoc-markdown.js
@@ -0,0 +1,23 @@
+const jsdoc2md = require('jsdoc-to-markdown')
+const path = require('path');
+const fs = require('fs');
+
+(function () {
+  const getRenderTemplateFromJSFile = async () => {
+    const jsMdData = await jsdoc2md.render({ files: 'src/*.js' })
+    return jsMdData;
+  }
+
+  const writeMdToReadMeFile = async () => {
+    try {
+      const data = await getRenderTemplateFromJSFile();
+      console.log(typeof data);
+      fs.writeFileSync(path.resolve(__dirname, '../README.md'), data);
+      console.log('写入成功');
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  writeMdToReadMeFile();
+})();
